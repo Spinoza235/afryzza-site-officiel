db526b6c768c337cef5eef95cea3e0e1cd20d46f
Spinoza235 <esperancedingammdji785@gmail.com>
2025-10-10 21:28:59 -0700

    Initial commit - React + TypeScript + Tailwind + Vite

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <h1 className="text-3xl bg-amber-400 p-4 font-bold underline rounded-b-4xl">
+       Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
